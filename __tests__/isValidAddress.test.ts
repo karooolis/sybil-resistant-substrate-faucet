d1aa5ad52dd2ed57a69092bc4910f7eef4ff64f4
karooolis <hello@karolisram.com>
2022-08-24 15:28:58 +0300

    Move coverage configts to jest.config.js

---
+++ __tests__/isValidAddress.test.ts
@@ -0,0 +1,14 @@
+import "@testing-library/jest-dom";
+import { isValidAddress } from "../utils/isValidAddress";
+
+describe("isValidAddress", () => {
+  test("valid address", () => {
+    expect(
+      isValidAddress("5GgiURgKaVw2nENZuUmLWQVV7oaGH7ryRkK4A7q4dZWNu69u")
+    ).toBe(true);
+  });
+
+  test("invalid address", () => {
+    expect(isValidAddress("this-is-invalid-address")).toBe(false);
+  });
+});
