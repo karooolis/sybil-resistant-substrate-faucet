2d07c0fc84c502814fa8a562640144349fced7d8
karooolis <hello@karolisram.com>
2022-09-07 21:13:49 +0300

    Start adding E2E playwright tests.

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,6 +54,8 @@ const Home = ({ claimed: initialClaimed }: Props) => {
     setLoading(false);
   };
 
+  console.log(process.env)
+
   return (
     <div className="max-w-7xl h-screen mx-auto px-4 sm:px-6 lg:px-8">
       <Head>
