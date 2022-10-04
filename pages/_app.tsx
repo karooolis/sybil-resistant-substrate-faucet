025e0e9694d9d5ac459c7b3525f342dc09e167ff
karooolis <hello@karolisram.com>
2022-10-04 14:02:54 +0300

    Add Session type to AppProps.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 
-function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
